58fa3353ba861a15704363779571800b66cebb41
Arpit <arpit200960632242@gmail.com>
2025-09-14 21:02:31 +0530

    first project done

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `
+<main>
+<div class="container">
+<img src="images/image-qr-code.png" title="frontend mentor QR Code">
+  <h1>Improve your front-end skills by building projects
+  </h1>
+
+  <p>
+  Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+  </p>
+</div>
+</main>
+`;
 
 setupCounter(document.querySelector('#counter'))
